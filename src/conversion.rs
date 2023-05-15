99474ed54757311ec5af4312491f56edf2f7cfae
Ryan Kavanagh <ryan.p.kav@gmail.com>
2023-05-15 23:35:03 +0100

    Setting up a new file with all the ascii chars setup

--- src/conversion.rs
+++ src/conversion.rs
@@ -1,16 +1,82 @@
-use Morsec::MorseChar; 
+use super::MorseChar;
 
-const A: &[MorseChar] = &[MorseChar::Dit, MorseChar::Dah];
-const B: &[MorseChar] = &[MorseChar::Dah, MorseChar::Dit, MorseChar::Dit, MorseChar::Dit];
-const C: &[MorseChar] = &[MorseChar::Dah, MorseChar::Dit, MorseChar::Dah, MorseChar::Dit];
-const D: &[MorseChar] = &[MorseChar::Dah, MorseChar::Dit, MorseChar::Dit];
-const E: &[MorseChar] = &[MorseChar::Dit];
-const F: &[MorseChar] = &[MorseChar::Dit, MorseChar::Dit, MorseChar::Dah, MorseChar::Dit];
-const G: &[MorseChar] = &[MorseChar::Dah, MorseChar::Dah, MorseChar::Dit];
-const H: &[MorseChar] = &[MorseChar::Dit, MorseChar::Dit, MorseChar::Dit, MorseChar::Dit];
-const I: &[MorseChar] = &[MorseChar::Dit, MorseChar::Dit];
-const J: &[MorseChar] = &[MorseChar::Dit, MorseChar::Dah, MorseChar::Dah, MorseChar::Dah];
-const K: &[MorseChar] = &[MorseChar::Dah, MorseChar::Dit, MorseChar::Dah];
-const L: &[MorseChar] = &[MorseChar::Dit, MorseChar::Dah, MorseChar::Dit, MorseChar::Dit];
-const M: &[MorseChar] = &[MorseChar::Dah, MorseChar::Dah];
-
+const A: [MorseChar; 2] = [MorseChar::Dit, MorseChar::Dah];
+const B: [MorseChar; 4] = [
+    MorseChar::Dah,
+    MorseChar::Dit,
+    MorseChar::Dit,
+    MorseChar::Dit,
+];
+const C: [MorseChar; 4] = [
+    MorseChar::Dah,
+    MorseChar::Dit,
+    MorseChar::Dah,
+    MorseChar::Dit,
+];
+const D: [MorseChar; 3] = [MorseChar::Dah, MorseChar::Dit, MorseChar::Dit];
+const E: [MorseChar; 1] = [MorseChar::Dit];
+const F: [MorseChar; 4] = [
+    MorseChar::Dit,
+    MorseChar::Dit,
+    MorseChar::Dah,
+    MorseChar::Dit,
+];
+const G: [MorseChar; 3] = [MorseChar::Dah, MorseChar::Dah, MorseChar::Dit];
+const H: [MorseChar; 4] = [
+    MorseChar::Dit,
+    MorseChar::Dit,
+    MorseChar::Dit,
+    MorseChar::Dit,
+];
+const I: [MorseChar; 2] = [MorseChar::Dit, MorseChar::Dit];
+const J: [MorseChar; 4] = [
+    MorseChar::Dit,
+    MorseChar::Dah,
+    MorseChar::Dah,
+    MorseChar::Dah,
+];
+const K: [MorseChar; 3] = [MorseChar::Dah, MorseChar::Dit, MorseChar::Dah];
+const L: [MorseChar; 4] = [
+    MorseChar::Dit,
+    MorseChar::Dah,
+    MorseChar::Dit,
+    MorseChar::Dit,
+];
+const M: [MorseChar; 2] = [MorseChar::Dah, MorseChar::Dah];
+const N: [MorseChar; 2] = [MorseChar::Dah, MorseChar::Dit];
+const O: [MorseChar; 3] = [MorseChar::Dah, MorseChar::Dah, MorseChar::Dah];
+const P: [MorseChar; 4] = [
+    MorseChar::Dit,
+    MorseChar::Dah,
+    MorseChar::Dah,
+    MorseChar::Dit,
+];
+const Q: [MorseChar; 4] = [
+    MorseChar::Dit,
+    MorseChar::Dit,
+    MorseChar::Dah,
+    MorseChar::Dit,
+];
+const R: [MorseChar; 3] = [MorseChar::Dit, MorseChar::Dah, MorseChar::Dit];
+const S: [MorseChar; 3] = [MorseChar::Dit, MorseChar::Dit, MorseChar::Dit];
+const T: [MorseChar; 1] = [MorseChar::Dah];
+const U: [MorseChar; 3] = [MorseChar::Dit, MorseChar::Dit, MorseChar::Dah];
+const V: [MorseChar; 4] = [
+    MorseChar::Dit,
+    MorseChar::Dit,
+    MorseChar::Dit,
+    MorseChar::Dah,
+];
+const W: [MorseChar; 3] = [MorseChar::Dit, MorseChar::Dah, MorseChar::Dah];
+const Y: [MorseChar; 4] = [
+    MorseChar::Dah,
+    MorseChar::Dit,
+    MorseChar::Dah,
+    MorseChar::Dah,
+];
+const Z: [MorseChar; 4] = [
+    MorseChar::Dah,
+    MorseChar::Dah,
+    MorseChar::Dit,
+    MorseChar::Dit,
+];
